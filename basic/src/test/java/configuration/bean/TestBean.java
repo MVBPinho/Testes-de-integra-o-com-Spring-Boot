40b1602340e6d6767c8e903bef15267ce62179de
MVBPinho <marcos.vb.pinho@live.com>
2022-02-28 18:21:48 -0300

    Adicionando configurações para os testes

---
+++ basic/src/test/java/configuration/bean/TestBean.java
@@ -0,0 +1,8 @@
+package configuration.bean;
+
+public class TestBean {
+
+	public String someMethod() {
+		return "I am not a real bean";
+	}
+}
---
+++ basic/src/test/java/configuration/bean/TestConfs.java
@@ -0,0 +1,13 @@
+package configuration.bean;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class TestConfs {
+
+	@Bean
+	public String moreOneBean() {
+		return "More one bean for tests";
+	}
+}
